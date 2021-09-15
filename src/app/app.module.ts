af82f0c55612b1c5569093580aaea662fee7f958
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2021-09-15 15:55:08 -0300

    Primeiro Commit - App Spring e Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
